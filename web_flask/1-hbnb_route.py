a5cbdc9b29f46897965cb445489bf0767cd547d2
stelmaks <stelmaks101@gmail.com>
2023-07-23 13:19:00 -0700

    HBNB is alive!

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,8 +8,16 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    """Return string when route queried
-    """
-    return 'Hello HBNB!'
+    """ First Route that display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ Second Route that display HBNB"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)'

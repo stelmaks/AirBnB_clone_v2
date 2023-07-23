98db515cec9f407abeff5426ea61b88060b80f77
stelmaks <stelmaks101@gmail.com>
2023-07-23 13:50:29 -0700

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
+"""Starts a flask web application with two routings
 """
-Script starts a Flask web application:
-listening on 0.0.0.0, port 5000
-With two Routes
-"""
-from flask import Flask, escape, request
+
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
-    """ First Route that display Hello HBNB"""
-    return "Hello HBNB!"
+    """Return string when route queried
+    """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """ Second Route that display HBNB"""
-    return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
+    """
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)'
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
